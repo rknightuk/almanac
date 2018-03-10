8d4fbb290c8a1e9e162c328b70aa16394e547044
Robb Lewis <me@therobb.com>
2018-03-10 11:41:58 +0000

    Setup command

---
+++ app/Console/Commands/AlamnacSetup.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Almanac\Console\Commands;
+
+use Almanac\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+class AlamnacSetup extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'almanac:setup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Setup Alamanc Database and User';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+	public function handle()
+	{
+		$this->setupDatabase();
+
+		if (User::first()) {
+			$this->warn('User account already created, aborting.');
+			return;
+		}
+
+		if (! $user = $this->createUser()) {
+			$this->info('Error: User creation failed. Please try again');
+			return;
+		}
+
+		$this->info('Setup successfully completed!');
+	}
+
+	private function setupDatabase()
+	{
+		if (Schema::hasTable('migrations'))
+		{
+			$this->warn('`migrate:install` already run, skipping');
+		} else {
+			$this->info('Running migration install');
+			Artisan::call('migrate:install');
+		}
+
+		if (Schema::hasTable('tweets'))
+		{
+			$this->warn('`migrate` already run, skipping');
+		} else {
+			$this->info('Running table migrations');
+			Artisan::call('migrate', ['--force' => true]);
+		}
+	}
+
+	private function createUser()
+	{
+		$this->info('Creating user account');
+
+		$email = $this->ask('Email Address');
+
+		try
+		{
+			Validator::make(['email' => $email], ['email' => 'email'])->validate();
+		} catch (ValidationException $e) {
+			$this->info('Error: Invalid email. aborting.');
+			return false;
+		}
+
+		$password = $this->secret('Password');
+		$confirmPassword = $this->secret('Confirm Password');
+
+		if ($password != $confirmPassword)
+		{
+			$this->info('Error: Passwords do not match. aborting.');
+			return false;
+		}
+
+		return User::create([
+			'email' => $email,
+			'password' => bcrypt($password)
+		]);
+	}
+}
