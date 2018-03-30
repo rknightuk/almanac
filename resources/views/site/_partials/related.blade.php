9968fcc3f1d865adafab894cbe8781e877df2191
Robb Lewis <me@therobb.com>
2018-03-30 09:16:01 +0100

    Tweaks to previous/future posts

--- resources/views/site/_partials/related.blade.php
+++ resources/views/site/_partials/related.blade.php
@@ -2,7 +2,7 @@
 	<div class="almn-post almn-post__related @if ($key === 0) almn-post__related--first-{{$type}} @endif almn-post__{{ $r->rating_string }}">
 		<div class="almn-post--titles--main almn-post--titles--main__related">
 			<a href="{{ $r->permalink }}">
-				{{ $r->title }} - {{ $r->date_completed->format('l jS F Y') }}
+				{{ ucfirst($r->verb) }} on {{ $r->date_completed->format('jS F Y') }}
 			</a>
 		</div>
 	</div>
