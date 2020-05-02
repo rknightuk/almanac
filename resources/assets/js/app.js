2ead100745957fe3e82b0ba08894928014c390b2
Robb Knight <desk@rknight.me>
2020-05-02 14:24:37 +0100

    Nuke all front end

--- resources/assets/js/app.js
+++
@@ -1,8 +0,0 @@
-require('./bootstrap');
-
-import React from 'react';
-import { render } from 'react-dom';
-
-import App from './src';
-
-if (document.getElementById('admin')) render(<App />, document.getElementById('admin'));
