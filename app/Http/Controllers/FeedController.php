2543fd72f708e0e68bb233e83247d097fbf144b4
Robb Knight <desk@rknight.me>
2020-05-02 08:15:37 +0100

    Remove feed for now

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -4,7 +4,6 @@ namespace Almanac\Http\Controllers;
 
 use Almanac\Posts\Post;
 use Illuminate\Support\Facades\URL;
-use Roumen\Feed\Feed;
 
 class FeedController extends Controller
 {
@@ -12,27 +11,28 @@ class FeedController extends Controller
 
     public function index()
     {
-    	$feed = new Feed();
-
-	    $feed->setCache(60, self::FEED_KEY);
-
-	    if (!$feed->isCached()) {
-		    $posts = Post::latest()->get();
-
-		    $feed->title = 'Almanac';
-		    $feed->description = 'A microblog for all the things I\'ve watched, read, and played';
-		    $feed->link = url('feed');
-		    $feed->setDateFormat('carbon');
-		    $feed->pubdate = $posts[0]->created_at;
-		    $feed->lang = 'en';
-		    $feed->setShortening(false);
-
-		    foreach ($posts as $post)
-		    {
-			    $feed->add($post->title, 'Robb Knight', URL::to($post->permalink), $post->created_at, $post->content, $post->content);
-		    }
-	    }
-
-    	return $feed->render('atom');
+        return; //todo
+//    	$feed = new Feed();
+//
+//	    $feed->setCache(60, self::FEED_KEY);
+//
+//	    if (!$feed->isCached()) {
+//		    $posts = Post::latest()->get();
+//
+//		    $feed->title = 'Almanac';
+//		    $feed->description = 'A microblog for all the things I\'ve watched, read, and played';
+//		    $feed->link = url('feed');
+//		    $feed->setDateFormat('carbon');
+//		    $feed->pubdate = $posts[0]->created_at;
+//		    $feed->lang = 'en';
+//		    $feed->setShortening(false);
+//
+//		    foreach ($posts as $post)
+//		    {
+//			    $feed->add($post->title, 'Robb Knight', URL::to($post->permalink), $post->created_at, $post->content, $post->content);
+//		    }
+//	    }
+//
+//    	return $feed->render('atom');
     }
 }
