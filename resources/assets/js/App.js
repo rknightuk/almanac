bfef21cf2038d970b19d084958d356ff863a1903
Robb Knight <desk@rknight.me>
2020-05-02 18:37:54 +0100

    React router

--- resources/assets/js/App.js
+++ resources/assets/js/App.js
@@ -2,14 +2,34 @@
 
 import * as React from 'react'
 import { hot } from 'react-hot-loader'
+import { BrowserRouter, Route } from 'react-router-dom'
 import css from './style.css'
+
+const Dashbaord = () => (
+    <div>Dashboard</div>
+)
+
+const Create = () => (
+    <div>Create</div>
+)
+
+const Update = () => (
+    <div>Update</div>
+)
 
 class App extends React.Component<{}> {
     render() {
         return (
-            <div>
-                This is <span className={css.red}> the main app!</span>
-            </div>
+            <BrowserRouter>
+                <div>
+                    <div style={{ maxWidth: '1000px', margin: '0 auto', padding: '0 20px' }}>
+                        <Route exact path="/app" component={Dashbaord}/>
+                        <Route exact path="/app/new" component={Create}/>
+                        <Route exact path="/app/new/:type/:id?" component={Create}/>
+                        <Route exact path="/app/posts/:id" component={Update}/>
+                    </div>
+                </div>
+            </BrowserRouter>
         )
     }
 }
