a51d7d593f8eeccbefd27256b202c226d7dbf8df
Robb Knight <desk@rknight.me>
2020-05-03 12:28:14 +0100

    Picker styles

--- resources/assets/src/Picker/index.js
+++ resources/assets/src/Picker/index.js
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Icon from 'src/ui/Icon'
 
 import { PostTypes } from 'src/types'
 
@@ -9,15 +10,19 @@ import css from './style.css'
 
 type BlockProps = {
     name: PostTypes,
-    title: string,
 }
 
-const Block = ({ name, title }: BlockProps) => (
+const Block = ({ name }: BlockProps) => (
     <Link
         to={`app/new/${name}`}
         className={css.block}
     >
-        <i class={`fas fa-${name}`} />
+        <div className={css.icon}>
+            <Icon
+                type={name}
+                grow="20"
+            />
+        </div>
     </Link>
 )
 
@@ -25,35 +30,27 @@ const Picker = () => (
     <div className={css.picker}>
         <Block
             name="movie"
-            title="Movie"
         />
         <Block
             name="tv"
-            title="TV"
         />
         <Block
             name="game"
-            title="Game"
         />
         <Block
             name="music"
-            title="Music"
         />
         <Block
             name="podcast"
-            title="Podcast"
         />
         <Block
             name="book"
-            title="Book"
         />
         <Block
             name="video"
-            title="Video"
         />
         <Block
             name="quote"
-            title="Quote"
         />
     </div>
 )
