8c5e7549dd5b04aaa684f7dd7c4d5b160fc07798
Robb Knight <desk@rknight.me>
2020-05-08 22:01:35 +0100

    Add tags to posts on seed

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Seeder;
 
 class PostSeeder extends Seeder
 {
+    const TAGS = [
+        'SHRT',
+        'Longer Tag',
+        'Another',
+    ];
+
     /**
      * Run the database seeds.
      *
@@ -16,6 +22,10 @@ class PostSeeder extends Seeder
     	factory(Post::class, 100)->create()->each(function(Post $post) {
     	    if (rand(0, 3) == 1) {
                 $post->attachments()->createMany(factory(Attachment::class, rand(1, 5))->make()->toArray());
+            }
+
+    	    if (rand(0, 2) === 1) {
+    	        $post->attachTags(self::TAGS);
             }
         });
     }
